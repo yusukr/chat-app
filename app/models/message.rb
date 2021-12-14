b8a6b926787f0cd0dddf7cd374e44733b21b28cd
yusukr <70104155+yusukr@users.noreply.github.com>
2021-12-15 08:13:24 +0900

    Actice Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ApplicationRecord
 
   belongs_to :user
   belongs_to :room
+  has_one_attached :image
 
-  validates :content, presence: true 
+  validates :content, presence: true
+  
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
+  has_many :messages, dependent: :destroy
   has_many :users, through: :room_users
 
   validates :name , presence: true
-  has_many :messages
 end
